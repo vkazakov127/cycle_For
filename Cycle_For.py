c2587ad46d9c189cf36135bd908fd60cf5a71634
vkazakov127 <166021972+vkazakov127@users.noreply.github.com>
2024-04-17 16:37:25 +0300

    Add files via upload
    Module 2. Lesson "Cycle FOR"

---
+++ Cycle_For.py
@@ -0,0 +1,6 @@
+my_cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for i in range(len(my_cars)):
+    print('Я езжу на автомобиле ', my_cars[i])
+    cars_count += 10
+    print('cars_count = ', cars_count)
